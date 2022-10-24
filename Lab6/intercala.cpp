708624c95ac6fd2d82ac2dc290c53c7ce4412fb2
analivi4 <slopeslivia@alu.ufc.br>
2022-10-24 16:17:39 -0300

    tentativa 2

--- Lab6/intercala.cpp
+++ Lab6/intercala.cpp
@@ -4,33 +4,42 @@ using namespace std;
 
 void mergeAB(int *v, int *a, int n, int *b, int m){
     
-    int i, j, k;
-    while (k<(m+n)){
-        while (i<n){
-            while(j<m){
-                if (a[i]<b[j]){
-                    v[k]=a[i];
-                    i++;
-                }
-            }
+    int i, j, k=0;
+    while (i<n && j<m){
+        if (a[i]<=b[j]){
+            v[k]=a[i];
+            k++;
+            i++;
+        }else{
+            v[k]=b[j];
+            k++;
+            j++;
         }
     }
-
-}
+    while(i<n){
+        v[k]=a[i];
+        k++; 
+        i++;
+    }
+    while(j<m){
+        v[k]=b[j];
+        k++;
+        j++;
     }
 
 }
 
 int main(){
 
-int v[100];
-int a[50];
-int b[50];
-
 int n,m;
 cin >> n >> m;
 
+int v[n+m];
+int a[n];
+int b[m];
+
 for(int i=0;i<n;i++){
+-4
 cin >> a[i];
 }
 
@@ -39,6 +48,7 @@ cin >> b[i];
 }
 
 mergeAB(v, a, n, b, m);
+
 for(int i=0;i<n+m;i++){
 cout << v[i];
 if(i!=n+m-1)
